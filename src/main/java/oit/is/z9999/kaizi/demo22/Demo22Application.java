51d9fce4a0386fd4ec7d5c0c2bb6ccf41f1da270
kiyoshi-honda <kiyoshi.honda@oit.ac.jp>
2022-11-05 16:13:54 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z9999/kaizi/demo22/Demo22Application.java
@@ -0,0 +1,13 @@
+package oit.is.z9999.kaizi.demo22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo22Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z9999/kaizi/demo22/Demo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z9999.kaizi.demo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
